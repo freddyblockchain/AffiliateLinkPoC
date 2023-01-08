794774b59f147a163fc2cb5bb0dd31e519b9cdbc
Frederik <frederikblockchain@gmail.com>
2023-01-08 22:52:15 +0100

    clean up project

--- src/algorand/algoClient.ts
+++ src/algorand/algoClient.ts
@@ -5,11 +5,4 @@ const port = "";
 const token = {
   "X-API-Key": "GCAE61OEML9V1hZKxCYf6EJBOGMHwvd15zmeM4Li",
 };
-
-export const APP_ID = 118516649;
-
-export const RANDOM_CONTRACT_ID = 110096026;
-
-export const LOTTERY_TOKEN_ID = 118517002;
-
 export const algodClient = new algosdk.Algodv2(token, baseServer, port);
--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,1 @@
-export type account = {
-  address: string;
-  mmemonic: string;
-};
 
-export type globalVars = {
-  [key: string]: number;
-};
-
-export type abiMethods =
-  | "startLottery"
-  | "resolveLottery"
-  | "participate"
-  | "resetLottery"
-  | "claimWin";
